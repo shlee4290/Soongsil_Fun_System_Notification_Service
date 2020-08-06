e82c6b7a02d81de4c158500f6b36d45b98ebf42a
이승현 <seunghyeon@MacBook-Pro.local>
2020-08-06 20:56:56 +0900

    API 서버 일부 기능 수정, Parsing app 주석 추가
    API 서버에서 프로그램 목록을 제공할 때 마감 임박순으로 정렬하여 제공하도록 수정하였음.
    Parsing app의 코드 일부분에 주석을 추가하였음, 정렬된 데이터 제공을 위해 프로그램의 레이블(remainLabel(String))도 DB에 저장하도록 하였음.

--- API_server/app.js
+++ API_server/app.js
@@ -3,14 +3,17 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // API-server
 // pwXyR7STuq19D3cZ
-mongoose.connect("mongodb+srv://API-server:pwXyR7STuq19D3cZ@cluster0.cxpu4.gcp.mongodb.net/funSystemParsingDB?retryWrites=true", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  "mongodb+srv://API-server:pwXyR7STuq19D3cZ@cluster0.cxpu4.gcp.mongodb.net/funSystemParsingDB?retryWrites=true",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const funProgramSchema = mongoose.Schema({
   title: String,
@@ -19,41 +22,67 @@ const funProgramSchema = mongoose.Schema({
   remainDate: String,
   url: String,
   id: Number,
-  isNewProgram: Boolean
-})
+  isNewProgram: Boolean,
+  remainLabel: String,
+});
 
 const FunProgram = mongoose.model("FunProgram", funProgramSchema);
 
 const versionSchema = mongoose.Schema({
-  version: Number
-})
+  version: Number,
+});
 
 const Version = mongoose.model("Version", versionSchema);
 
-app.get("/version", function(req, res) {
-  Version.findOne(function(err, foundVersion) {
-    if (!err){
+app.get("/version", function (req, res) {
+  Version.findOne(function (err, foundVersion) {
+    if (!err) {
       res.send(foundVersion);
     } else {
       res.send(err);
     }
-  })
-})
-
-app.get("/programs", function(req, res) {
-  FunProgram.find(function(err, foundPrograms) {
-    if (!err) {
-      res.send(foundPrograms);
-    } else {
-      res.send(err);
-    }
-  })
-})
-
-app.listen(3333, function(){
-  console.log("Soongsil Fun System Parsing data API server is now running on port 3333");
+  });
 });
 
+app.get("/programs", function (req, res) {
+  FunProgram.aggregate(
+    [
+      { $project: 
+        {
+          "title": 1,
+          "department": 1,
+          "date": 1,
+          "remainDate": 1,
+          "url": 1,
+          "id": 1,
+          "isNewProgram": 1,
+          "remainLabel": 1, 
+          "length": { "$strLenCP": "$remainDate" } 
+        } 
+      },
+      {$sort: 
+        {
+          "remainLabel": 1,
+          "length": 1,
+          "remainDate": 1
+        }
+      }
+    ],
+      function (err, foundPrograms) {
+        if (!err) {
+          res.send(foundPrograms);
+        } else {
+          res.send(err);
+        }
+      } 
+    );
+});
+
+app.listen(3333, function () {
+  console.log(
+    "Soongsil Fun System Parsing data API server is now running on port 3333"
+  );
+});
 
 // /version
 // /programs
